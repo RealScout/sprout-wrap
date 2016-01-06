d85f8a6400e7571d2368a658c1c84beaf9f3a177
Chris Conley <pair+chris@realscout.com>
2016-01-05 16:41:22 -0800

    first stab at new cookbook

---
+++ site-cookbooks/rs-osx-settings/metadata.rb
@@ -0,0 +1,12 @@
+name             'rs-osx-settings'
+maintainer       'RealScout'
+maintainer_email 'chris@realscout.com'
+license          'MIT'
+description      'Recipes to configure various aspects of OS X'
+long_description 'hi'
+version          '0.1.0'
+supports         'mac_os_x'
+depends          'sprout-base'
+depends          'homebrew'
+depends          'osx'
+
