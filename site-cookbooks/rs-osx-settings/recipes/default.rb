2fa6dcee26c12a692397196aa4a89bc5bc0990d7
Chris Conley <pair+chris@realscout.com>
2016-01-05 17:21:37 -0800

    add key repeat

--- site-cookbooks/rs-osx-settings/recipes/default.rb
+++ site-cookbooks/rs-osx-settings/recipes/default.rb
@@ -23,3 +23,15 @@ osx_defaults "disable smart dashes" do
   key 'NSAutomaticDashSubstitutionEnabled'
   boolean false
 end
+
+osx_defaults "fast key repeat" do
+  domain 'NSGlobalDomain'
+  key 'KeyRepeat'
+  integer 2
+end
+
+osx_defaults "fast initial key repeat" do
+  domain 'NSGlobalDomain'
+  key 'InitialKeyRepeat'
+  integer 15
+end
